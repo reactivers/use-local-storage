62bf731d565a8aaefbb6dde6151430074c18c795
Murat Güney <muratguney@Murat-MacBook-Pro.local>
2022-05-28 16:58:25 +0300

    dist is removed

--- dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as LocalStorageProvider } from "./hooks/useLocalStorage/context";
-export { default as useLocalStorage } from "./hooks/useLocalStorage";
--- dist/types/utils/functions.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const isBrowser: () => boolean;
-export declare const tryJSONparse: (obj: any) => any;
-export declare const tryJSONStringify: (obj: any) => any;
